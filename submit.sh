da9023099051c015f2a4c3c07e69cc7e59d9aac0
Glastonbury Group <caglastonbury@gmail.com>
2024-08-22 22:10:31 +0200

    example submit.sh

---
+++ submit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=omero_download
+#SBATCH --output=logs/omero_download_%A_%a.out  # Output file for each job
+#SBATCH --error=logs/omero_download_%A_%a.err   # Error file for each job
+#SBATCH --time=12:00:00                    # Maximum execution time (hh:mm:ss)
+#SBATCH --mem=4G                           # Memory per job
+#SBATCH --cpus-per-task=4                  # Number of CPUs per job
+#SBATCH --array=1-9                   # Array range and concurrency (adjust based on number of CSV files)
+
+CSV_DIR="omero_2566_wsi_chunks"
+CSV_FILES=($CSV_DIR/*.csv)
+CSV_FILE=${CSV_FILES[$SLURM_ARRAY_TASK_ID]}
+
+python download_from_omero.py --csv_file omero_2566_wsi_chunks/$CSV_FILE --dataset_id 2566
